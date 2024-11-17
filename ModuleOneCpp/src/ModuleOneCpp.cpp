1e1555af699ad9d0fd6db94e0a26addf70278dad
ulianaboikova <ulyanaboykova49@gmail.com>
2024-11-16 21:19:03 -0800

    Initial Commit

---
+++ ModuleOneCpp/src/ModuleOneCpp.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : ModuleOneCpp.cpp
+// Author      : Uliana Boikova
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to this C++ Program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+
+    return 0;
+
+}
